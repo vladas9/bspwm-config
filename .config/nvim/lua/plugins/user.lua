60d79ce10563fc0bd392c3e35a2dfe3a15658ec1
vladas9 <vladamza63@gmail.com>
2024-05-31 15:58:49 +0300

    feat: Add nvim config

---
+++ .config/nvim/lua/plugins/user.lua
@@ -0,0 +1,25 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- You can also add or configure plugins by creating files in this `plugins/` folder
+-- Here are some examples:
+
+---@type LazySpec
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+  },
+  {
+    "goolord/alpha-nvim",
+    opts = function(_, opts)
+      -- customize the dashboard header
+      opts.section.header.val = {
+        "    ███    ██ ██    ██ ██ ███    ███",
+        "    ████   ██ ██    ██ ██ ████  ████",
+        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
+        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
+        "    ██   ████   ████   ██ ██      ██",
+      }
+      return opts
+    end,
+  },
+}
