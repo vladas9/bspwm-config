339a78ef27e35b82ba7c41d78f610ca77d90238f
vladas9 <vladamza63@gmail.com>
2024-06-03 23:03:39 +0300

    fix: change polybar workspace menu

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -8,6 +8,5 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.recipes.disable-tabline" },
   -- import/override with your plugins folder
 }
